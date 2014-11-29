c84904a53f5d4f301361be3cedbd10e49ac0beb6
didjoman <alexrupp@free.fr>
2014-11-29 19:30:39 +0100

    ajout de l'attente de la fin des threads dans le main + des mutex dans tcp-tcp.c

--- src/tsp-tsp.h
+++ src/tsp-tsp.h
@@ -5,6 +5,6 @@
 extern int minimum;
 
 int present (int city, int hops, tsp_path_t path);
-void tsp (int hops, int len, tsp_path_t path, long long int *cuts, tsp_path_t sol, int *sol_len);
+void tsp (int hops, int len, tsp_path_t path, long long int *cuts, tsp_path_t sol, int *sol_len, pthread_mutex_t m);
 
 #endif
